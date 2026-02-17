6d89edeef0063c1871840f3816083bba20707861
rajeshh21 <rajeshh21@users.noreply.github.com>
2026-02-18 01:07:08 +0530

    fix typescript error

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ export const metadata = {
   description: 'Connect with travelers around the world',
 }
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
